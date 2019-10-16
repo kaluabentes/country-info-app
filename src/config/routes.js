0020fb50c3dc85f697a4f06f2324bd9ae60a4597
Kaluã Bentes <kaluabentes@gmail.com>
2019-10-16 14:26:57 -0400

    Create boilerplate

---
+++ src/config/routes.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    path: '/',
+    text: 'Home',
+  },
+  {
+    path: '/about',
+    text: 'About',
+  },
+  // {
+  //   path: '/resume-creator',
+  //   text: 'Resume Creator',
+  // },
+]
